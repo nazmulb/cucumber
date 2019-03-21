6afe5e9e1a2ed9b50bc97c459461fda8e121c854
nazmulbasher <nazmul.basher@fieldnation.com>
2019-03-21 12:25:54 +0600

    Changed some contents

--- cucumber.js
+++ cucumber.js
@@ -1,4 +1,6 @@
-//var common = '--require-module @babel/register';
+// Default command line arguments
+// In order to prevent users from having to enter the options they use every time
+// Users can define cucumber.js with profiles which are groups of command line arguments.
 
 module.exports = {
     'default': '--format summary',
